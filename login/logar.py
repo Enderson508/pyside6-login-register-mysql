6a7803a82312c9587f6ed06e7fb3ba5fd0d6ed8e
Enderson508 <endersonvieira409@gmail.com>
2025-05-13 09:38:58 -0300

    Configurações de login e registro

---
+++ login/logar.py
@@ -0,0 +1,198 @@
+import sys
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout,
+    QPushButton, QLineEdit, QTextBrowser, QMessageBox, QSpacerItem, QSizePolicy
+)
+from PySide6.QtCore import Qt
+from .register import registerWindow
+from page.page import JanelaVazia
+import bcrypt
+import mysql.connector
+
+
+class loginWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+    
+    def setupUI(self):
+
+        self.label = QLabel(self)
+        self.label.setFixedSize(500,500)
+        self.label.setStyleSheet(
+            "background-color: #121414;" \
+            " border: 2px solid white;" \
+            " border-radius: 30px;" \
+            " color: white;"
+            )
+        
+        #CENTRALIZANDO LABEL
+        self.centralwidget = QWidget()
+        self.setCentralWidget(self.centralwidget)
+        
+        self.main_layout = QVBoxLayout(self.centralwidget)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        self.content_layout = QVBoxLayout()
+        self.content_layout.setSpacing(20)
+        self.content_layout.setContentsMargins(0, 20, 0, 0)
+        self.content_layout.setContentsMargins(20, 20, 0, 0)
+        self.content_layout.addSpacerItem(QSpacerItem(20, 10))
+
+
+        # TÍTULO
+        self.fonte_title = QTextBrowser()
+        self.fonte_title.setText("<font size='20'><b>LOGIN</b></font>")
+        self.fonte_title.setStyleSheet(
+            'background-color: transparent;'
+            ' color: white;'
+            ' border: None;'
+        )
+        self.fonte_title.setMaximumHeight(50)
+
+        self.content_layout.addWidget(self.fonte_title, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.content_layout.setContentsMargins(20, 20, 0, 0)
+
+        # Espaço entre título e campos
+        self.content_layout.addSpacerItem(QSpacerItem(20, 50))
+
+        # USUÁRIO
+        self.fonte_user = QLineEdit()
+        self.fonte_user.setPlaceholderText("ㅤDigite o nome de usuário")
+        self.fonte_user.setMaxLength(12)
+        self.fonte_user.setStyleSheet(
+            'background-color: #121414;'
+            ' color: white;'
+            ' border: 2px solid white;'
+            ' border-radius: 18px;'
+        )
+        self.fonte_user.setFixedHeight(41)
+        self.fonte_user.setFixedWidth(270)
+
+        # SENHA
+        self.fonte_password = QLineEdit()
+        self.fonte_password.setPlaceholderText("ㅤDigite a senha")
+        self.fonte_password.setEchoMode(QLineEdit.EchoMode.Password)
+        self.fonte_password.setMaxLength(12)
+        self.fonte_password.setStyleSheet(
+            'background-color: #121414;'
+            ' color: white;'
+            ' border: 2px solid white;'
+            ' border-radius: 18px;'
+        )
+        self.fonte_password.setFixedHeight(41)
+        self.fonte_password.setFixedWidth(270)
+
+        self.content_layout.addWidget(self.fonte_user, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.content_layout.addSpacerItem(QSpacerItem(10,10))
+        self.content_layout.addWidget(self.fonte_password, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.content_layout.addSpacerItem(QSpacerItem(10, 10))
+
+        # BOTÃO ENTRAR
+        self.button = QPushButton("Entrar")
+        self.button.setStyleSheet(
+            'background-color: #121414;'
+            ' font-weight: bold;'
+            ' color: white;'
+            ' border: 2px solid white;'
+            ' border-radius: 18px;'
+        )
+        self.button.setFixedHeight(41)
+        self.button.setFixedWidth(270)
+        self.button.clicked.connect(self.login)
+
+        # BOTÃO REGISTRO
+        self.button_register = QPushButton("Registre-se")
+        self.button_register.setStyleSheet(
+            'background-color: transparent;'
+            ' color: white;'
+            ' font-weight: bold;'
+            ' border: none;'
+        )
+        self.button_register.setFixedHeight(30)
+        self.button_register.setFixedWidth(100)
+        self.button_register.clicked.connect(self.showregisterpage)
+        
+        register_layout = QHBoxLayout()
+        register_layout.setContentsMargins(0, 0, 0, 0)
+        register_layout.addSpacerItem(QSpacerItem(0, 0))
+        register_layout.addWidget(self.button_register, alignment=Qt.AlignmentFlag.AlignLeft)
+
+        self.content_layout.addLayout(register_layout)
+        self.content_layout.addWidget(self.button, alignment=Qt.AlignmentFlag.AlignLeft)
+
+        
+        self.content_layout.addSpacerItem(QSpacerItem(20, 10, QSizePolicy.Minimum, QSizePolicy.Expanding))
+
+        wrapper_layout = QVBoxLayout()
+        wrapper_layout.addLayout(self.content_layout)
+
+    
+        self.label.setLayout(wrapper_layout)
+        self.main_layout.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignCenter)
+        
+        # CONFIG DA JANELA
+        self.setWindowTitle("Login")
+        self.setMinimumSize(800, 600)
+        self.resize(1280, 720)
+        self.setStyleSheet(
+            "background: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1, \
+            stop: 0 #0d0d23, stop: 1 #121414);"
+        )
+
+        # BANCO DE DADOS
+        self.conexao = mysql.connector.connect(
+            host="localhost",
+            username="", #COLOQUE O NOME DE USUARIO DO SERVIDOR MYSQL
+            password="", #COLOQUE A SENHA(SE TIVER)
+            database="" #COLOQUE O NOME DO SEU BANCO DE DADOS
+        )
+        self.cursor = self.conexao.cursor()
+    
+    #FUNÇÃO PARA ABRIR TELA DE REGISTRO
+    def showregisterpage(self):
+        self.register = registerWindow()
+        self.register.show()
+        self.close()
+    
+    #FUNÇÃO PARA ABRIR JANELA
+    def loginok(self):
+        self.janela = JanelaVazia()
+        self.janela.setupWINDOW()
+        
+    #FUNÇÃO PARA PROCURAR USUÁRIO   
+    def founduser(self, login_name):
+        comandos = "SELECT * FROM sua_table WHERE nome = %s" #COLOQUE A TABLE DO SEU BANCO DE DADOS E O VALOR QUE REPRESENTE O NOME DE USUÁRIO
+        valores = (login_name,)
+        self.cursor.execute(comandos, valores)
+        return self.cursor.fetchone()
+    
+    #CONFIGURANDO PROCESSO DE LOGIN
+
+    def login(self):
+        user = self.fonte_user.text()
+        senha = self.fonte_password.text()
+        resultado = self.founduser(user)
+
+        if not user or not senha:
+            QMessageBox.warning(self, "ERRO!", "CAMPO VAZIO!")
+            return
+
+        if resultado is None:
+            QMessageBox.warning(self, "ERRO!", "USUÁRIO OU SENHA INCORRETOS!")
+            return
+
+        senha_hash = resultado[2].encode('utf8')
+
+        if bcrypt.checkpw(senha.encode('utf8'), senha_hash):
+            QMessageBox.information(self, "SUCESSO!", "LOGIN BEM SUCEDIDO!")
+            self.loginok()
+            self.close()
+        else:
+            QMessageBox.warning(self, "ERRO!", "USUÁRIO OU SENHA INCORRETOS!")
+
+
+app = QApplication(sys.argv)
+window = loginWindow()
+window.show()
+sys.exit(app.exec())
---
+++ login/register.py
@@ -0,0 +1,221 @@
+import sys
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout,
+    QPushButton, QLineEdit, QTextBrowser, QMessageBox, QSpacerItem, QSizePolicy
+)
+from PySide6.QtCore import Qt, QRect
+import bcrypt
+import mysql.connector
+
+class registerWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUII()
+
+    
+    def setupUII(self):
+        
+        self.label = QLabel(self)
+        self.label.setFixedSize(500,500)
+        self.label.setStyleSheet(
+            "background-color: #121414;" \
+            " border: 2px solid white;" \
+            " border-radius: 30px;" \
+            " color: white;"
+            )
+        
+        #CENTRALIZANDO LABEL
+        self.centralwidget = QWidget()
+        self.setCentralWidget(self.centralwidget)
+        
+        self.main_layout = QVBoxLayout(self.centralwidget)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        self.content_layout = QVBoxLayout()
+        self.content_layout.setSpacing(20)
+        self.content_layout.setContentsMargins(0, 20, 0, 0)
+        self.content_layout.setContentsMargins(20, 20, 0, 0)
+        self.content_layout.addSpacerItem(QSpacerItem(20, 10))
+        
+        #TITULO DE REGISTRO
+        self.fonte_title = QTextBrowser(self)
+        self.fonte_title.setText(
+            "<font size = '20'><b>REGISTER</b><font>"
+            )
+        self.fonte_title.setStyleSheet(
+            'background-color: transparent;' \
+            ' color: white;' \
+            ' border: None;'
+            )
+        self.fonte_title.setFixedSize(250,100)
+        self.content_layout.addWidget(self.fonte_title, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.content_layout.addSpacerItem(QSpacerItem(10, 10))
+        
+        
+        #AREAS DE DIGITAÇÃO
+        self.fonte_user = QLineEdit(self)
+       
+        self.fonte_user.setObjectName(u"lineEdit")
+        self.fonte_user.setFixedSize(250, 41)
+        self.fonte_user.setPlaceholderText("ㅤDigite o nome de usuário")
+        self.fonte_user.setMaxLength(12)
+        self.fonte_user.setStyleSheet(
+            'background-color: #121414;' \
+            ' color: white; border: 2px solid white;' \
+            ' border-radius: 18px;'
+            )
+        
+        self.content_layout.addWidget(self.fonte_user, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.content_layout.addSpacerItem(QSpacerItem(20, 20))
+        
+       
+        #SENHAS
+        
+        self.fonte_senha = QLineEdit(self)
+        self.fonte_senha.setObjectName(u"lineEdit")
+        self.fonte_senha.setFixedSize(250, 41)
+        self.fonte_senha.setPlaceholderText("ㅤDigite a senha que você deseja")
+        self.fonte_senha.setEchoMode(QLineEdit.EchoMode.Password)
+        self.fonte_senha.setMaxLength(12)
+        self.fonte_senha.setStyleSheet(
+            'background-color: #121414;' \
+            ' color: white; border:' \
+            ' 2px solid white;' \
+            ' border-radius: 18px;')
+        self.content_layout.addWidget(self.fonte_senha, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.content_layout.addSpacerItem(QSpacerItem(5, 5))
+        
+        
+
+        self.fonte_senha_confirm = QLineEdit(self)
+        self.fonte_senha_confirm.setObjectName(u"lineEdit")
+        self.fonte_senha_confirm.setFixedSize(250, 42)
+        self.fonte_senha_confirm.setPlaceholderText("ㅤConfirme sua senha")
+        self.fonte_senha_confirm.setEchoMode(QLineEdit.EchoMode.Password)
+        self.fonte_senha_confirm.setMaxLength(12)
+        self.fonte_senha_confirm.setStyleSheet(
+            'background-color: #121414;' \
+            ' color: white;' \
+            ' border: 2px solid white;' \
+            ' border-radius: 18px;'
+            )
+        self.content_layout.addWidget(self.fonte_senha_confirm, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.content_layout.addSpacerItem(QSpacerItem(10, 10))
+              
+        
+    
+        
+
+        #BOTÃO DE ENTRAR
+        self.button = QPushButton(self)
+        self.button.setText("Registrar")
+        self.button.setFixedSize(200, 41)
+        self.button.setStyleSheet(
+            'background-color: #121414;' \
+            ' font-weight: bold;' \
+            ' color: white;' \
+            ' border: 2px solid white;' \
+            ' border-radius: 18px;'
+            )
+        self.button.clicked.connect(self.register)
+        
+        self.content_layout.addWidget(self.button, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.content_layout.addSpacerItem(QSpacerItem(20, 10, QSizePolicy.Minimum, QSizePolicy.Expanding))
+        
+        self.wrapper = QVBoxLayout()
+        self.wrapper.addLayout(self.content_layout)
+        
+        self.label.setLayout(self.wrapper)
+        
+        self.main_layout.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignCenter)
+
+
+ 
+        #CONFIGURAÇÃO DA JANELA
+        self.setWindowTitle("Register")
+        self.setMinimumSize(800, 600)
+        self.resize(1280, 720)
+        self.show() 
+        self.setStyleSheet(
+            "background: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1, \
+            stop: 0 #0d0d23,\
+            stop: 1 #121414);"
+            )
+        
+
+
+        #SERVIDOR MYSQL
+
+        self.conexao = mysql.connector.connect(
+            host = "localhost",
+            username = "", #COLOQUE O NOME DE USUARIO DO SERVIDOR MYSQL
+            password = "", #COLOQUE A SENHA(SE TIVER)
+            database = "" #COLOQUE O NOME DO SEU BANCO DE DADOS
+        )
+        self.cursor = self.conexao.cursor(buffered=True)
+
+    #INSERIR VALORES NO BANCO DE DADOS
+    def registeruser(self, user_name, user_senha):
+        senha_hash = bcrypt.hashpw(user_senha.encode('utf-8'), bcrypt.gensalt())
+        comandos = "INSERT INTO sua_table(dado_user, dado_password) VALUES (%s, %s)" #COLOQUE A SUA TABLE E DENTRO DO PARÊNTESES COLOQUE OS VALORES QUE REPRESENTAM O NOME E A SENHA DE USUÁRIO
+        valores =   (user_name, senha_hash) 
+        self.cursor.execute(comandos, valores)
+        self.conexao.commit()
+        
+    #PROCURA VALORES NO BANCO DE DADOS
+    def founduser(self, user):
+        comandos = "SELECT * FROM sua_table WHERE nome = %s" #COLOQUE A TABLE DO SEU BANCO DE DADOS E O VALOR QUE REPRESENTE O NOME DE USUÁRIO
+        valores = (user,)
+        self.cursor.execute(comandos, valores)
+        return self.cursor.fetchone() is not None
+        
+        
+
+    #ABRIR TELA DE LOGIN
+    def registerok(self):
+        from .logar import loginWindow
+        self.windowregister = loginWindow()
+        self.windowregister.show()
+        self.close()  
+
+
+    #CONFIGURANDO ÁREA DE REGISTRO
+    def register(self):
+        user = self.fonte_user.text()
+        senha = self.fonte_senha.text() 
+
+        if user == "" or senha == "" or self.fonte_senha_confirm.text() == "":
+            QMessageBox.warning(self, "ERRO!", "CAMPO VAZIO!")
+            return
+        
+
+        if self.fonte_senha.text() != self.fonte_senha_confirm.text():
+            QMessageBox.warning(self,"ERRO!", "SENHAS DIFERENTES!")
+            return
+
+        if self.founduser(user):
+            QMessageBox.warning(self, "ERRO!", "NOME DE USUÁRIO JÁ EXISTE!")
+            return 
+        
+        self.registeruser(user, senha)
+        QMessageBox.information(self,
+             "SUCESSO!", 
+             "USUÁRIO CADASTRADO COM SEUCESSO!"
+             ) 
+        self.registerok()  
+        
+
+  
+
+
+  
+      
+            
+            
+if __name__ == "__main__":
+    app = QApplication([])
+    window = registerWindow()
+    window.show()
+    app.exec() 
+
+
